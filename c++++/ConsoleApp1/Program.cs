3ad1c9cca5072c0eaeaa1bf34396c1ded3281c1c
4snt <murilo.escobedo@protonmail.com>
2024-01-27 20:21:23 -0300

    "primeira aula:)"

---
+++ c++++/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class principal
+    {
+        static void Main()
+        {
+            Console.Write("Olá mundo\n");
+            Console.Write("eu sou Murilo, Prazer em conhecer você c#");
+            Console.ReadLine();
+        }
+    }
+}
